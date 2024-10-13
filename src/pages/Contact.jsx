35db4d8c450040f311f25fdacdb5a01243c41e22
Pawel Walicki <pawel.walicki89@interia.pl>
2024-10-13 14:15:10 +0200

    instaling icon mui, add contact data

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+import { Button} from "@mui/material"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import './Contact.css'
 export const Contact = () => {
     return (
-        <div>
-            There will be Contact page in the future!!
+        <div className="contactBox">           
+           <a className="icons" href="https://github.com/PawelWalicki/movies-app"><GitHubIcon fontSize="large"/></a>
+           <a className="icons" href="https://www.linkedin.com/in/paweł-walicki-a39b09176/"><LinkedInIcon fontSize="large"/></a>
+        
+            <div className="buttonHome">
+                <Link to={"/"}>
+                    <Button variant='contained'>Home</Button>
+                </Link>
+            </div>
         </div>
     )
 }
